503f96fcbe9f9d9b05d150f96495ce0703f92741
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-08 15:08:03 +0900

    プロジェクト設定ファイルを分割

---
+++ syokutomo/syokutomo/settings_dev.py
@@ -0,0 +1,48 @@
+from .settings_common import
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# ロギング設定
+LOGING = {
+    'version': 1, # 1固定
+    'disable_existing_loggers':False,
+
+    # ロガーの設定
+    'loggers':{ 
+        #Djangoが利用するロガー
+        'django': {
+            'handlers':['console'],
+            'level':'INFO',
+        },
+        #syokutomoアプリケーションが利用するロガー
+        'syokutomo': {
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    },
+    
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format':'\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
