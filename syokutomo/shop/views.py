cbc68d7705f14ffa4293740b8e52d98225892f9d
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-09 11:42:55 +0900

    shop_mypage追加

--- syokutomo/shop/views.py
+++ syokutomo/shop/views.py
@@ -3,3 +3,6 @@ from django.views import generic
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "shop_index.html"
+
+class MypageView(generic.TemplateView):
+    template_name = "shop_mypage.html"
