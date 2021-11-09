33b32c5d69774d9d5cdcb33c0eba0d172ca4cda1
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-09 11:49:38 +0900

    shop/urls.py 編集

--- syokutomo/shop/urls.py
+++ syokutomo/shop/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
-    path('/mypage',views.MypageView.as_view(),name="mypage")
+    path('mypage',views.MypageView.as_view(),name="mypage")
 ]
