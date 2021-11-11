0ad41d48c5c3c49a04b2d4121802c4e7437ca07e
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-11 14:10:20 +0900

    user_mypage追加

--- syokutomo/user/urls.py
+++ syokutomo/user/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
-    path('',views.IndexView.as_view(),name="index"),
+    path('', views.IndexView.as_view(), name="index"),
+    path('mypage', views.MypageView.as_view(), name="mypage"),
+    path('terms', views.TermsView.as_view(), name="terms"),
 ]
--- syokutomo/user/views.py
+++ syokutomo/user/views.py
@@ -1,5 +1,15 @@
 from django.views import generic
 
 # Create your views here.
+
+
 class IndexView(generic.TemplateView):
     template_name = "user_index.html"
+
+
+class MypageView(generic.TemplateView):
+    template_name = "user_mypage.html"
+
+
+class TermsView(generic.TemplateView):
+    template_name = "user_terms.html"
