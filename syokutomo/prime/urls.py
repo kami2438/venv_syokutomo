bac8bd6bfaa397741325a72763989aadb2d0ba29
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-12 11:36:00 +0900

    prime/urls.py_9行目追加

--- syokutomo/prime/urls.py
+++ syokutomo/prime/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'prime'
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
+    path('choice_login',views.Choice_loginView.as_view(),name='choice_login'),
 ]
