6f4e7f8ec0ea86583b9c4df6e8e5897cd1ebd062
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-12 10:42:56 +0900

    カスタムユーザー作成

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class CustomUser(AbstractUser):
+    type_choice=(
+        ("1", "ユーザー"),
+        ("2", "店舗"),
+        ("3", "配達員"),)
+    user_type=models.IntegerField(verbose_name_='user_type',choices=type_choice)
+    class Meta:
+        verbose_name_plural="CustomUser"
