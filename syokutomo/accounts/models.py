1836bdd5e7bc2403d3f77cd7686138eaeb732835
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-12 10:43:55 +0900

    カスタムユーザー修正

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -6,6 +6,6 @@ class CustomUser(AbstractUser):
         ("1", "ユーザー"),
         ("2", "店舗"),
         ("3", "配達員"),)
-    user_type=models.IntegerField(verbose_name_='user_type',choices=type_choice)
+    user_type=models.IntegerField(verbose_name='user_type',choices=type_choice)
     class Meta:
         verbose_name_plural="CustomUser"
