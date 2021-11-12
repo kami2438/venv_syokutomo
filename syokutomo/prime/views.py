6f0883e3d49d89f422f2d703fe3b035ca7d535f6
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-12 11:38:55 +0900

    prime/views.py_6~8行目追加

--- syokutomo/prime/views.py
+++ syokutomo/prime/views.py
@@ -3,3 +3,6 @@ from django.views import generic
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class Choice_loginView(generic.TemplateView):
+    template_name = 'choice_login.html'
