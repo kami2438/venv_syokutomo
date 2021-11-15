5bb13d5ca8e40957b8959b59ff5345056d3c72b3
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-15 10:17:58 +0900

    prime利用規約接続

--- syokutomo/prime/urls.py
+++ syokutomo/prime/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 app_name = 'prime'
 urlpatterns = [
-    path('',views.IndexView.as_view(),name="index"),
-    path('choice_login',views.Choice_loginView.as_view(),name='choice_login'),
+    path('', views.IndexView.as_view(), name="index"),
+    path('choice_login', views.Choice_loginView.as_view(), name='choice_login'),
+    path('terms', views.TermsView.as_view(), name="terms"),
 ]
--- syokutomo/prime/views.py
+++ syokutomo/prime/views.py
@@ -1,8 +1,15 @@
 from django.views import generic
 
 # Create your views here.
+
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
+
 class Choice_loginView(generic.TemplateView):
     template_name = 'choice_login.html'
+
+
+class TermsView(generic.TemplateView):
+    template_name = "prime_terms.html"
