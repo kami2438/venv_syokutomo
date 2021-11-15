85aa81cab314bc92afa57fd00f500ead71845c5b
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 10:44:02 +0900

    カンマ取り消し

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -5,7 +5,7 @@ class CustomUser(AbstractUser):
     type_choice=(
         ("1", "ユーザー"),
         ("2", "店舗"),
-        ("3", "配達員"),)
+        ("3", "配達員"))
     user_type=models.IntegerField(verbose_name='user_type',choices=type_choice)
     class Meta:
         verbose_name_plural="CustomUser"
