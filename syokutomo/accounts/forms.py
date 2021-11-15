316a821f8d077edc93b0ce4a84edf12345052d10
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 10:34:10 +0900

    type_choice　クラス名追記

--- syokutomo/accounts/forms.py
+++ syokutomo/accounts/forms.py
@@ -1,18 +1,15 @@
 # forms.py
 from allauth.account.forms import SignupForm
 from django import forms
-from .import models
-
-
-class CustomSignupForm(SignupForm):  # SignupFormを継承する
+import models
+class CustomSignupForm(SignupForm): #SignupFormを継承する
     forms.fields.ChoiceField(
-        choices=type_choice.choices,
+        choices=CustomUser.type_choice.choices,
         required=True,
         label='タイプ',
         # widget=forms.widgets.Select,
     )
-    user_type = form
-
+    user_type=form
     def signup(self, request, user):
         user.user_type = self.cleaned_data['user_type']
         user.save()
