8b6f8291e33c7aa942f46153cb67bb38b862b598
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 14:13:54 +0900

    ふぉｒｍ付け足し

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django import forms
 # Create your models here.
 class CustomUser(AbstractUser):
     type_choice=(
