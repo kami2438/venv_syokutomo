b781715a62376a6620165578cde1ade9a3e015aa
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 14:15:09 +0900

    ウィジット削除

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
         ("1", "ユーザー"),
         ("2", "店舗"),
         ("3", "配達員"))
-    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1, widget=forms.widgets.Select)
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1, required=True)
     nickname = models.CharField('ニックネーム', max_length=50)
     class Meta:
         verbose_name_plural="CustomUser"
