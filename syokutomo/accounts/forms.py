8a214f9fca587b197f984705dedca5eec1ade161
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 14:39:08 +0900

    forms 変更

--- syokutomo/accounts/forms.py
+++ syokutomo/accounts/forms.py
@@ -5,16 +5,21 @@ from .models import CustomUser
 
 
 class CustomSignupForm(SignupForm):  # SignupFormを継承する
-    forms.fields.ChoiceField(
-        choices=CustomUser.type_choice,
-        required=True,
-        label='タイプ'
-        # widget=forms.widgets.Select,
-    )
+    # forms.fields.ChoiceField(
+    #     choices=CustomUser.type_choice,
+    #     required=True,
+    #     label='タイプ'
+    #     # widget=forms.widgets.Select,
+    # )
     # last_name = forms.CharField(max_length=30, label='名字')
     # user_type = form
+    user_type= forms.CharField(
+        widget=forms.Select(choices=CustomUser.type_choice))
+    class Meta:
+        model=CustomUser
 
-    def signup(self, request, user):
+    def signup(self, request):
+        user= super(CustomSignupForm, self).save(request)
         user.user_type = self.cleaned_data['user_type']
         # user.last_name = self.cleaned_data['last_name']
         user.save()
