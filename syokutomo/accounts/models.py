c16a8df2509e453243be566d851db868903bc927
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-15 13:58:25 +0900

    カンマ付け足し

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -6,6 +6,6 @@ class CustomUser(AbstractUser):
         ("1", "ユーザー"),
         ("2", "店舗"),
         ("3", "配達員"))
-    user_type=models.CharField(verbose_name='user_type',choices=type_choice　max_length=1)
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1)
     class Meta:
         verbose_name_plural="CustomUser"
