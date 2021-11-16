4dd5b5695a2f0a02ea0e517a540d87ec4ca7aad1
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-16 15:59:25 +0900

    判定逆転

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
         ("2", "店舗"),
         ("3", "配達員"))
     user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
-    main_regist= models.BooleanField(default=True)
+    main_regist= models.BooleanField(default=False)
     # nickname = models.CharField('ニックネーム', max_length=50,null=True)
     class Meta:
         verbose_name_plural="CustomUser"
