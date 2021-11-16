edfca40e63b96ce9e2418aa3551911be98ea1396
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-11-16 14:56:57 +0900

    formメール送信機能追加

--- syokutomo/prime/forms.py
+++ syokutomo/prime/forms.py
@@ -20,3 +20,22 @@ class ReservationForm(forms.Form):
 
         self.fields['message'].widget.attrs['class'] = 'form-control col-12'
         self.fields['message'].widget.attrs['placeholder'] = 'メッセージをここに入力してください。'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ {}'.format(title)
+        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, message)
+        from_email = 'admin@example.com'
+        to_list = [
+            'test@example.com'
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
--- syokutomo/prime/views.py
+++ syokutomo/prime/views.py
@@ -1,8 +1,13 @@
+import logging
+
+from django.urls import reverse_lazy
+
 from django.views import generic
 
 from .forms import ReservationForm
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -19,3 +24,10 @@ class TermsView(generic.TemplateView):
 class ReservationView(generic.FormView):
     template_name = "prime_reservation.html"
     form_class = ReservationForm
+    success_url = reverse_lazy('syokutomo:reservation')
+
+    def form_valid(self, form):
+    form.send_email()
+    messages.success(self.request, 'メッセージを送信しました。')
+    logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+    return super().form_valid(form)
