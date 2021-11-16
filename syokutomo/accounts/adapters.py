2b0ae8c71f8065ca136884f084633db4ce76e1c5
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-16 12:19:34 +0900

    アダプタ変更

--- syokutomo/accounts/adapters.py
+++ syokutomo/accounts/adapters.py
@@ -2,11 +2,13 @@ from allauth.account.adapter import DefaultAccountAdapter
 from django.urls import reverse_lazy
 
 class MyCrmAdapter(DefaultAccountAdapter):
-    def get_login_redirect_url(self, request):
-        resolved_url = super().get_login_redirect_url(request)
-        user_obj = request.user
-        try:
-            groupsettings = user_obj.groupsettings
-        except:
-            resolved_url = reverse_lazy("settings_form")
-        return resolved_url
+def save_user(self, request, user, form, commit=True):
+        """
+        This is called when saving user via allauth registration.
+        We override this to set additional data on user object.
+        """
+        # Do not persist the user yet so we pass commit=False
+        # (last argument)
+        user = super(AccountAdapter, self).save_user(request, user, form, commit=False)
+        user.user_type = form.cleaned_data.get('user_type')
+        user.save()
