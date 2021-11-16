718db6b33a00d3df8672fdfb709941c1fcaa2ce5
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-16 11:48:05 +0900

    タイプ初期値設定

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
         ("1", "ユーザー"),
         ("2", "店舗"),
         ("3", "配達員"))
-    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1)
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
     # nickname = models.CharField('ニックネーム', max_length=50,null=True)
     class Meta:
         verbose_name_plural="CustomUser"
