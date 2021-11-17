d9c5c092aa8b4d0ad4e45417f68bf3b1f6dd9338
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-17 14:00:09 +0900

    prime/urls.py_12~14追加

--- syokutomo/prime/urls.py
+++ syokutomo/prime/urls.py
@@ -6,7 +6,10 @@ from . import views
 app_name = 'prime'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('choice_login', views.Choice_loginView.as_view(), name='choice_login'),
-    path('terms', views.TermsView.as_view(), name="terms"),
-    path('reservation', views.ReservationView.as_view(), name="reservation"),
+    path('choice_login/', views.Choice_loginView.as_view(), name='choice_login'),
+    path('terms/', views.TermsView.as_view(), name="terms"),
+    path('reservation/', views.ReservationView.as_view(), name="reservation"),
+    path('main_regis_user/', views.Prime_main_regis_userViews.as_view(), name=",main_regis_user"),
+    path('main_regis_shop/', views.Prime_main_regis_shopViews.as_view(), name=",main_regis_shop"),
+    path('main_regis_driver/', views.Prime_main_regis_driverViews.as_view(), name=",main_regis_driver"),
 ]
