f114b4d02ba4a90502627327aac3da0543ac70fc
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-25 14:59:25 +0900

    shop/views.py_ListView

--- syokutomo/shop/views.py
+++ syokutomo/shop/views.py
@@ -9,3 +9,11 @@ class MypageView(generic.TemplateView):
 
 class TermsView(generic.TemplateView):
     template_name = "shop_terms_of_service.html"
+
+class ListView(LoginRequiredMixin, generic.ListView):
+    model = T1_shop
+    template_name = 'shop_list.html'
+
+    def get_queryset(self):
+        shops = T1_shop.object.filter(user=self.request.user).order_by('-created_at')
+        return shop
