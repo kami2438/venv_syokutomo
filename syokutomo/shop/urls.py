47b20bb9ea8fb4ef0aa7ceadcdd7e7d5c5657fb1
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-26 10:51:17 +0900

    shop/urls.py_変更

--- syokutomo/shop/urls.py
+++ syokutomo/shop/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
     path('mypage/',views.MypageView.as_view(),name="mypage"),
     path('terms/',views.TermsView.as_view(),name="terms"),
-    path('list/',views.ListView.as_view(),name="list"),
-    
+
 ]
