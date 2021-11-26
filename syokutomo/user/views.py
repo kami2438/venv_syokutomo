bb1b548e19c53b735b86dfb9a8e84731d62a041f
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-26 10:50:03 +0900

    user/views.py_変更

--- syokutomo/user/views.py
+++ syokutomo/user/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views import generic
+
+from prime.models import T1_shop
 
 # Create your views here.
 
@@ -13,3 +17,11 @@ class MypageView(generic.TemplateView):
 
 class TermsView(generic.TemplateView):
     template_name = "user_terms.html"
+
+class ListView(LoginRequiredMixin, generic.ListView):
+    model = T1_shop
+    template_name = 'user_shoplist.html'
+
+    def get_queryset(self):
+        users = T1_shop.object.filter(user=self.request.user).order_by('-created_at')
+        return users
