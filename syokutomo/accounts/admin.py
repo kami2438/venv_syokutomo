efc3120cd77f1edb3aafdfa7682301f3d2ceb55a
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 12:41:08 +0900

    いmp追加

--- syokutomo/accounts/admin.py
+++ syokutomo/accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-
+from import_export import resources
+from import_export.admin import ImportExportMixin
 from .models import *
 class t10Admin(ImportExportMixin, admin.ModelAdmin):
     list_display = ['t10_area_prefecture', 't10_area_name', 't10_create_at', 't10_update_at']
