49d5ea2c79f15e25a78218cfd812b731617aba5f
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 12:40:20 +0900

    admin変更

--- syokutomo/accounts/admin.py
+++ syokutomo/accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import CustomUser
-
+from .models import *
+class t10Admin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['t10_area_prefecture', 't10_area_name', 't10_create_at', 't10_update_at']
+admin.site.register(T10_area, t10Admin)
 admin.site.register(CustomUser)
--- syokutomo/prime/admin.py
+++ syokutomo/prime/admin.py
@@ -3,11 +3,10 @@ from .models import *
 # Register your models here.
 from import_export import resources
 from import_export.admin import ImportExportMixin
-from accounts.models import CustomUser
-from .models import T10_area
+from accounts.models import *
 
-class t10Admin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ['t10_area_prefecture', 't10_area_name', 't10_create_at', 't10_update_at']
+
+
 
 class t9Admin(ImportExportMixin,admin.ModelAdmin):
     list_display=["t9_food_category_name","t9_create_at","t9_update_at" ]
@@ -15,7 +14,7 @@ class t9Admin(ImportExportMixin,admin.ModelAdmin):
 class t8Admin(ImportExportMixin,admin.ModelAdmin):
     list_display=["t8_shop_category_name","t8_create_at","t8_update_at" ]
 
-admin.site.register(T10_area, t10Admin)
+
 admin.site.register(T9_food_category,t9Admin)
 
 
