32883553c33b96208857c1498d23c740d22392ed
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 11:31:36 +0900

    元に戻しt

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -1,5 +1,16 @@
 from django.db import models
-
-from prime.models import *
-
+from django.contrib.auth.models import AbstractUser
+from django import forms
+from prime.models import T10_area
 # Create your models here.
+class CustomUser(AbstractUser):
+    type_choice=(
+        ("1", "ユーザー"),
+        ("2", "店舗"),
+        ("3", "配達員"))
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
+    main_regist= models.BooleanField(default=False)
+    area_id= models.ForeignKey(T10_area,verbose_name='エリア',on_delete= models.SET_DEFAULT,default='未設定')
+    # nickname = models.CharField('ニックネーム', max_length=50,null=True)
+    class Meta:
+        verbose_name_plural="CustomUser"
--- syokutomo/prime/models.py
+++ syokutomo/prime/models.py
@@ -4,27 +4,17 @@ from django.db import models
 from django.db import models
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 from django.contrib.postgres.fields import ArrayField
-# from accounts.models import CustomUser
+from accounts.models import CustomUser
 from django.utils import timezone
 
 from django.core.validators import RegexValidator, MaxValueValidator ,MinValueValidator
 from import_export import resources
-from django.contrib.auth.models import AbstractUser
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Create your models here.
+
+
+# number_only=RegexValidator(regex='^[0-9]{1,}]$', message='数字だけ入力ください')
+
+#foreign key 先のクラスを先に初期化
 class T10_area(models.Model):
    
     t10_area_prefecture=models.CharField(verbose_name='県',max_length=70,blank=False)
@@ -33,22 +23,6 @@ class T10_area(models.Model):
     t10_update_at=models.DateTimeField(verbose_name='最終更新日時',auto_now=True)
     def __str__(self):
         return " %s %s" % (self.t10_area_prefecture,self.t10_area_name)
-
-class CustomUser(AbstractUser):
-    type_choice=(
-        ("1", "ユーザー"),
-        ("2", "店舗"),
-        ("3", "配達員"))
-    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
-    main_regist= models.BooleanField(default=False)
-    area_id= models.ForeignKey(T10_area,verbose_name='エリア',on_delete= models.SET_DEFAULT,default='未設定')
-    # nickname = models.CharField('ニックネーム', max_length=50,null=True)
-    class Meta:
-        verbose_name_plural="CustomUser"
-
-# number_only=RegexValidator(regex='^[0-9]{1,}]$', message='数字だけ入力ください')
-
-#foreign key 先のクラスを先に初期化
 
 class T8_shop_category(models.Model):
  
@@ -228,4 +202,3 @@ class T13_inquiry(models.Model):
     t13_sentence=models.TextField(verbose_name='内容',max_length=400,blank=False)
     t13_create_at=models.DateTimeField(verbose_name='作成日時',auto_now_add=True)
     t13_update_at=models.DateTimeField(verbose_name='最終更新日時',auto_now=True)
-
