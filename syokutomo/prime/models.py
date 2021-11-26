5993f5892149f604e7b9f5edc6b7357d7f2b0f92
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 11:28:49 +0900

    位置変更

--- syokutomo/prime/models.py
+++ syokutomo/prime/models.py
@@ -26,15 +26,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
-
-
-
-
-
-
-
-
+class CustomUser(AbstractUser):
+    type_choice=(
+        ("1", "ユーザー"),
+        ("2", "店舗"),
+        ("3", "配達員"))
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
+    main_regist= models.BooleanField(default=False)
+    area_id= models.ForeignKey(T10_area,verbose_name='エリア',on_delete= models.SET_DEFAULT,default='未設定')
+    # nickname = models.CharField('ニックネーム', max_length=50,null=True)
+    class Meta:
+        verbose_name_plural="CustomUser"
 
 # number_only=RegexValidator(regex='^[0-9]{1,}]$', message='数字だけ入力ください')
 
@@ -227,14 +229,3 @@ class T13_inquiry(models.Model):
     t13_create_at=models.DateTimeField(verbose_name='作成日時',auto_now_add=True)
     t13_update_at=models.DateTimeField(verbose_name='最終更新日時',auto_now=True)
 
-class CustomUser(AbstractUser):
-    type_choice=(
-        ("1", "ユーザー"),
-        ("2", "店舗"),
-        ("3", "配達員"))
-    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
-    main_regist= models.BooleanField(default=False)
-    area_id= models.ForeignKey(T10_area,verbose_name='エリア',on_delete= models.SET_DEFAULT,default='未設定')
-    # nickname = models.CharField('ニックネーム', max_length=50,null=True)
-    class Meta:
-        verbose_name_plural="CustomUser"
