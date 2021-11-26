77197e9d34ccd80346bfcbfa6822e2798c7dbc0f
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-11-26 10:50:47 +0900

    shop/views.py_変更

--- syokutomo/shop/views.py
+++ syokutomo/shop/views.py
@@ -1,8 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-
 from django.views import generic
-
-from prime.models import T1_shop
 
 # Create your views here.
 class IndexView(generic.TemplateView):
@@ -14,10 +10,3 @@ class MypageView(generic.TemplateView):
 class TermsView(generic.TemplateView):
     template_name = "shop_terms_of_service.html"
 
-class ListView(LoginRequiredMixin, generic.ListView):
-    model = T1_shop
-    template_name = 'shop_listing.html'
-
-    def get_queryset(self):
-        shops = T1_shop.object.filter(user=self.request.user).order_by('-created_at')
-        return shop
