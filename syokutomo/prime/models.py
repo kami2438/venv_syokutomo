8f5e71334688358ee6c861cf009597c8b5f6aea1
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 11:27:43 +0900

    ユーザー移転

--- syokutomo/prime/models.py
+++ syokutomo/prime/models.py
@@ -4,12 +4,36 @@ from django.db import models
 from django.db import models
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 from django.contrib.postgres.fields import ArrayField
-from accounts.models import CustomUser
+# from accounts.models import CustomUser
 from django.utils import timezone
 
 from django.core.validators import RegexValidator, MaxValueValidator ,MinValueValidator
 from import_export import resources
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Create your models here.
+
+
+
+
+
+
+
+
+
 
 
 # number_only=RegexValidator(regex='^[0-9]{1,}]$', message='数字だけ入力ください')
@@ -202,3 +226,15 @@ class T13_inquiry(models.Model):
     t13_sentence=models.TextField(verbose_name='内容',max_length=400,blank=False)
     t13_create_at=models.DateTimeField(verbose_name='作成日時',auto_now_add=True)
     t13_update_at=models.DateTimeField(verbose_name='最終更新日時',auto_now=True)
+
+class CustomUser(AbstractUser):
+    type_choice=(
+        ("1", "ユーザー"),
+        ("2", "店舗"),
+        ("3", "配達員"))
+    user_type=models.CharField(verbose_name='user_type',choices=type_choice,max_length=1,default="1")
+    main_regist= models.BooleanField(default=False)
+    area_id= models.ForeignKey(T10_area,verbose_name='エリア',on_delete= models.SET_DEFAULT,default='未設定')
+    # nickname = models.CharField('ニックネーム', max_length=50,null=True)
+    class Meta:
+        verbose_name_plural="CustomUser"
