8463dd0302c4cc35375d22dcf79decde01345030
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-11-26 11:36:56 +0900

    printテスト

--- syokutomo/accounts/models.py
+++ syokutomo/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django import forms
+print(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa)
 from prime.models import T10_area
 # Create your models here.
 class CustomUser(AbstractUser):
