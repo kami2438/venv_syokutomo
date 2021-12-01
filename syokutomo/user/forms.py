2da4b7f751a5ed918ef6b558ab7de060c6dcf430
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-12-01 12:15:46 +0900

    ,消し

--- syokutomo/user/forms.py
+++ syokutomo/user/forms.py
@@ -4,7 +4,7 @@ from .models import *
 
 class User_UpdateForm(forms.ModelForm):
     class Meta:
-        model = T5_user,
+        model = T5_user
         fields = ("t5_user_firstname", "t5_user_lastname", "t5_address", "t5_post", "t5_tel_number", "t5_landmark", "t5_allergy", "t5_charge_tool",
                   "t5_credit_number", "t5_credit_limit", "t5_bank_name", "t5_bank_location", "t5_bank_number", "t5_bank_password")
         labels = {"t5_user_firstname":"苗字", "t5_user_lastname":"名前", "t5_address":"住所", "t5_post":"郵便番号", "t5_tel_number":"電話番号",
