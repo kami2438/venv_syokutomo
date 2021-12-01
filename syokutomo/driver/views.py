464b83f2f5f7dd1aaddf94845d16d23db7db1702
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-12-01 14:49:51 +0900

    ドライバーアカウント詳細画面

--- syokutomo/driver/views.py
+++ syokutomo/driver/views.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.postgres import fields
+from django.urls import reverse_lazy
+from django.urls.base import reverse
+from django.contrib import messages
+
 from django.views import generic
+
+from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -7,12 +17,20 @@ class IndexView(generic.TemplateView):
     template_name = "driver_index.html"
 
 
-class MypageView(generic.TemplateView):
+class MypageView(LoginRequiredMixin, generic.ListView):
     template_name = "driver_mypage.html"
-
+    model =T7_delivery_man
+    def get_queryset(self):
+        informations = T7_delivery_man.objects.filter(user=self.request.user)
+        return informations
 
 class TermsView(generic.TemplateView):
     template_name = "driver_terms.html"
 
 class StatusView(generic.TemplateView):
     template_name = "driver_status.html"
+
+
+class driver_infoView(LoginRequiredMixin, generic.DetailView):
+    model = T7_delivery_man
+    template_name = "driver_info.html"
