9e7465a9e443450bebfb648c67c054943166fa19
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-12-01 09:56:32 +0900

    アカウント詳細画面作成

--- syokutomo/user/urls.py
+++ syokutomo/user/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('list/', views.ListView.as_view(), name="list"),
     path('mypage/info/<int:pk>/',
          views.user_informationView.as_view(), name="info"),
+    # path('mypage/info/<int:pk>/',
+    #      views.user_informationView.as_view(), name="info"),
 ]
