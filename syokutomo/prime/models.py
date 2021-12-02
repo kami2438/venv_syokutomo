6d9464cc4d87166913096bc9d9652f8a41d73b9d
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-12-02 09:59:39 +0900

    user変更

--- syokutomo/prime/models.py
+++ syokutomo/prime/models.py
@@ -191,7 +191,7 @@ class T11_love(models.Model):
 class T12_charge(models.Model):
     # t12_charge_id=models.CharField(verbose_name='お気に入りID',primary_key=True,max_length=20,validators=[RegexValidator(regex=r"^C[0-9]*$")])
    
-    t5_user_id=models.ForeignKey(T5_user,verbose_name='店舗ID',on_delete=models.CASCADE,null=False)
+    user=models.ForeignKey(CustomUser,verbose_name='ユーザー',on_delete=models.PROTECT)
     t12_charge_amount=models.PositiveIntegerField(verbose_name='チャージ額',blank=False,default=0)
     t12_create_at=models.DateTimeField(verbose_name='作成日時',auto_now_add=True)
     t12_update_at=models.DateTimeField(verbose_name='最終更新日時',auto_now=True)
