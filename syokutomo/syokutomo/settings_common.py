1f6a1a62239e4a0f0cf3bc307cc1ddd2c9ad9d2d
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-12-02 14:40:22 +0900

    コレクトスタティック

--- syokutomo/syokutomo/settings_common.py
+++ syokutomo/syokutomo/settings_common.py
@@ -133,16 +133,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # if static_root error showed uncommand this. static_root is new parameter at django 3
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 # django-allauthで利用するdjango.contrib.sitesを使うためにサイト識別用IDを設定
