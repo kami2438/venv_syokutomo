5824b71dfe31fea02e47070e75ce7177cb7551e7
kami2438 <92900057+kami2438@users.noreply.github.com>
2021-12-02 11:28:15 +0900

    wrap_boolean_check追加

--- syokutomo/shop/forms.py
+++ syokutomo/shop/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import *
 
+def wrap_boolean_check(v):
+    return not (v is False or v is None or v == '' or v == 0)
+
 class shop_infoForm(forms.ModelForm):
     class Meta:
         model = T1_shop
