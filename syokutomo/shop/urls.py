ee9432c4d6cda64210532979aebaf75c95f7f06b
pokerface-009 <93752257+pokerface-009@users.noreply.github.com>
2021-12-03 10:43:27 +0900

    コメントアウト

--- syokutomo/shop/urls.py
+++ syokutomo/shop/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
     path('mypage/',views.MypageView.as_view(),name="mypage"),
     path('terms/',views.TermsView.as_view(),name="terms"),
-    path('merchandise/', views.MerchandiseView.as_view(),name="merchandise"),
+#    path('merchandise/', views.MerchandiseView.as_view(),name="merchandise"),
 ]
