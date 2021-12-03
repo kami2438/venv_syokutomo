71510d02923224aae04ef2a80471e1e51c939f26
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2021-12-03 14:44:41 +0900

    create at

--- syokutomo/user/views.py
+++ syokutomo/user/views.py
@@ -91,7 +91,7 @@ class user_ChargeView(LoginRequiredMixin, generic.CreateView):
         messages.success(self.request,'チャージされました。')
         chuser=T5_user.objects.filter(
             user=self.request.user)[0]
-        charge=T12_charge.objects.all().order_by('-created_at')[0]
+        charge=T12_charge.objects.all().order_by('t12_create_at')[0]
         n=int(charge.t12_charge_amount)+int(chuser.t5_charge_remain)
         chuser.t5_charge_remain=n
         charge.t12_charge_remain_ex=n
