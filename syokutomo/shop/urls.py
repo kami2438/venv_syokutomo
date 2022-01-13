32f461b8e6f1f33b15f9f89d14d823c6720a4e99
oharastudent2002067 <93752973+oharastudent2002067@users.noreply.github.com>
2022-01-13 09:58:44 +0900

    foodlist_url

--- syokutomo/shop/urls.py
+++ syokutomo/shop/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
     path('mypage/',views.MypageView.as_view(),name="mypage"),
     path('terms/',views.TermsView.as_view(),name="terms"),
-#    path('merchandise/', views.MerchandiseView.as_view(),name="merchandise"),
-    # path('food-create/',views.FoodCreateView.as_view(),name='food_create'),
-    # path('food-list/',views.FoodListView.as_view(),name='food_list')
     path('mypage/info/<int:pk>/',
      views.shop_infoView.as_view(), name="info"),
     path('mypage/info/shop_update/<int:pk>/',
      views.shop_updateView.as_view(), name="shop_update"),
+    path('food-list/',views.FoodListView.as_view(),name='food_list'),
+    path('food-detail/<t1_shop_id>/',views.FoodDetailView.as_view(),name='food_detail'),
+    path('food-update/<int:pk>/',views.FoodUpdateView.as_view(),name='food_update'),
+    path('food-delete/<int:pk>/',views.FoodDeleteView.as_view(),name='food_delete'),
 ]
