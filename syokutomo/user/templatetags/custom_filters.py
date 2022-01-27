6503a235d2bec20d31a51d71ed1ae26443ab18de
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-01-27 16:05:03 +0900

    vhf

--- syokutomo/user/templatetags/custom_filters.py
+++ syokutomo/user/templatetags/custom_filters.py
@@ -4,7 +4,13 @@ register = template.Library()
 
 @register.filter
 def checked(value, querydict):
-    kinds = querydict.getlist('kind')
+    kinds = querydict.getlist('category')
     if str(value) in kinds:
         return "checked"
     return ""
+
+@register.filter
+def name(querydict):
+    name = querydict.get('name')
+    
+    return "" if name is None else name
