c25ce8cc82e456ede074d92d4619aced78849a33
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-01-27 11:46:23 +0900

    データ確認

--- syokutomo/user/views.py
+++ syokutomo/user/views.py
@@ -128,6 +128,10 @@ class user_productView(LoginRequiredMixin, generic.DetailView):
                     done.delete()
             print("save")
             return reverse_lazy('user:product', kwargs={'pk': self.kwargs['pk']})
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests: instantiate a blank version of the form."""
+        return self.render_to_response(self.get_context_data())
     
 
     # def get(self,request,*args,**kwargs):
