0d9ab574d180e695f9d03dcbe3f102d5a8ad2f3f
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-01-28 09:58:23 +0900

    ｆｇｈｊｋ

--- syokutomo/user/templatetags/custom_filters.py
+++ syokutomo/user/templatetags/custom_filters.py
@@ -3,11 +3,17 @@ from django import template
 register = template.Library()
 
 @register.filter
+# def checked(value, querydict):
+#     kinds = querydict.get('category')
+#     print(kinds)
+#     print("hhhh",value)
+#     # return "" if name is None else name
+
 def checked(value, querydict):
-    kinds = querydict.get('category')
-    print(kinds)
-    print("hhhh",value)
-    # return "" if name is None else name
+    kinds = querydict.getlist('category')
+    if str(value) in kinds:
+        return "checked"
+    return ""
 
 @register.filter
 def name(querydict):
