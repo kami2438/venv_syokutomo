fd3351bfa80136a2459b1617e88cb825d54f546d
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-01-31 15:56:56 +0900

    rgth

--- syokutomo/user/forms.py
+++ syokutomo/user/forms.py
@@ -81,15 +81,13 @@ class OrderDetailForm(forms.ModelForm):
         ('22:00:00', '22:00'),('22:30:00', '22:30'),
         ('23:00:00', '23:00'),('23:30:00', '23:30') )
         fields = '__all__'
-        
-        # t4_food_id=
         exclude=['user','t2_order_id','t1_shop_id','t3_review_done',"t3_order_deliver_status","t7_delivery_man_id","t3_create_at"
         "t3_update_at","t3_payment"]
         widgets={"t3_delivery_date":forms.Select(choices=TIME_CHOICES),
         # "t1_start_time":,"t1_end_time":
         }
-    def __init__( self, *args,a):
-        super(OrderDetailForm,self).__init__(*args)
+    def __init__( self, *args, **kwargs):
+        super(OrderDetailForm,self).__init__(*args, **kwargs)
         # id = kwargs.get("instance").user.id
         ord=T2_order.objects.filter(id=self.kwargs['pk'])
         self.fields['t4_food_id'].queryset = T4_food.objects.filter(t1_shop_id=ord.t1_shop_id)
