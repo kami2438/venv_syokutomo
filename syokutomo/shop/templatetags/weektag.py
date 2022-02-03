46a48e3c1cda939f6acefb7c3abd088226b27997
kohssy0857 <54564343+kohssy0857@users.noreply.github.com>
2022-02-03 11:45:21 +0900

    カスタムフィルター的yぷ

--- syokutomo/shop/templatetags/weektag.py
+++ syokutomo/shop/templatetags/weektag.py
@@ -4,5 +4,9 @@ register = template.Library() # Djangoのテンプレートタグライブラリ
 
 # カスタムフィルタとして登録する
 @register.filter
-def multiply(value1, value2):
-    return value1 * value2
+def cusweek(value):
+    weeks={1:"日",2:"月",3:"火",4:"水",5:"木",6:"金",7:"土"}
+    if value in weeks:
+        week = weeks[value]
+    return week
+
